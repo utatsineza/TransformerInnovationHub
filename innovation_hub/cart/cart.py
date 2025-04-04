f3f1f956111e597c2e93081843d6cf052193d100
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-04-04 12:50:18 +0200

    .

--- innovation_hub/cart/cart.py
+++ innovation_hub/cart/cart.py
@@ -11,14 +11,15 @@ class Cart():
         # Now that the session key exists, get the cart from session
         self.cart = self.session['session_key']
 
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
-
+        product_qty = str(quantity)
         # Add product to cart if not already there
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
+            # self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
 
         # Mark session as modified
         self.session.modified = True
@@ -33,3 +34,28 @@ class Cart():
 
         # returning products in cart
         return products
+
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
+
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+
+        # update cart
+        store_cart = self.cart
+        store_cart[product_id] = product_qty
+
+        self.session.modified = True
+        new_cart = self.cart
+        return new_cart
+
+    def delete(self, product):
+        product_id = str(product)
+
+        # delete product from dictionary
+        if product in self.cart:
+            del self.cart[product_id]
+
+        self.session.modified = True
--- innovation_hub/cart/views.py
+++ innovation_hub/cart/views.py
@@ -9,8 +9,10 @@ def cart_summary(request):
     cart = Cart(request)
 
     cart_products = cart.get_prods()
-    return render(request, "cart.html", {"cart_products": cart_products})
-
+    quantities = cart.get_quants()
+    return render(
+        request, "cart.html", {"cart_products": cart_products, "quantities": quantities}
+    )
 
 
 def cart_add(request):
@@ -18,28 +20,49 @@ def cart_add(request):
 
     if request.POST.get("action") == "post":
         product_id = request.POST.get("product_id")
+        product_qty = request.POST.get("product_qty")
 
-        if not product_id:
+        if not product_qty:
             print("Got an empty string!")
             return HttpResponse("Got empty string")
 
-        product_id = int(product_id)
-
+        # product_id = int(product_id)
+        # product_qty = int(product_qty)
         # Get product from DB
         product = get_object_or_404(Product, id=product_id)
 
         # Add product to cart
-        cart.add(product)
+        cart.add(product=product, quantity=product_qty)
 
         # Get the updated cart quantity
-        cart_quantity = len(cart)
+        cart_quantity = cart.__len__()
 
         # Send updated cart quantity as a JSON response
         return JsonResponse({"qty": cart_quantity})
 
-def cart_delete(request):
-    pass
 
+def cart_delete(request):
+    cart = Cart(request)
+
+    if request.POST.get("action") == "post":
+        product_id = request.POST.get("product_id")
+
+        # calling the delete function
+        cart.delete(product=product_id)
+
+        response = JsonResponse({"product": product_id})
+        return response
+        return redirect("cart_summary")
 
 def cart_update(request):
-    pass
+    cart = Cart(request)
+
+    if request.POST.get("action") == "post":
+        product_id = request.POST.get("product_id")
+        product_qty = request.POST.get("product_qty")
+
+        cart.update(product=product_id, quantity=product_qty)
+
+        response = JsonResponse({"qty": product_qty})
+        return response
+        return redirect("cart_summary")
